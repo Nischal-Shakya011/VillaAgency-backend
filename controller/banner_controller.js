b3e38c268e1aac80c290edbde0acfbbaa05a70a3
Nischal Shakya <shakyanischal1111@gmail.com>
2024-05-31 00:10:30 +0545

    new

--- controller/banner_controller.js
+++ controller/banner_controller.js
@@ -7,7 +7,7 @@ const createBanner = async(req, res, next) =>{
         let images = [];
         // console.log(req.files);
         
-          req.files.images?.forEach(img => {
+          req.files.images?.forEach(img =>{
             let img_name = Date.now() + '-' + Math.round(Math.random() * 1E9) + img.name; //from multer to make name of pictures different
             let img_res = img.mv(path.join(__dirname, '../uploads/' + img_name)) //to uploads the pictures on "uploads" file
             // console.log(img_res)
